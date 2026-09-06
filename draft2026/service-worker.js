04fd4bf15134ca221376a69496a85917540fbeaa
kev11wolf <kev11wolf@gmail.com>
2026-09-06 14:27:57 -0400

    Fix recommendation card formatting; add collapsible cards, live status strip, and pick toasts

--- draft2026/service-worker.js
+++ draft2026/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='draft-command-2026-v5';
+const CACHE='draft-command-2026-v6';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
