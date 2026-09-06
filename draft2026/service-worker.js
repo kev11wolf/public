b8d0fd18f3b8754d87dd48dff172015714f8e1d5
kev11wolf <kev11wolf@gmail.com>
2026-09-06 01:46:06 -0400

    Add category breakdown, player profile modal, and unique ADP-based ranking

--- draft2026/service-worker.js
+++ draft2026/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='draft-command-2026-v2';
+const CACHE='draft-command-2026-v4';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
