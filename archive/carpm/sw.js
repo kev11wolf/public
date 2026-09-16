38aa8572c7087c8c15e6259c7656567675c1347b
kev11wolf <kev11wolf@gmail.com>
2026-09-16 13:18:47 -0400

    Connect Garage PM PWA to vehicle guide data

--- archive/carpm/sw.js
+++ archive/carpm/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "garage-pm-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./vehicles-index.json", "./2019-vw-tiguan-s-fwd.json", "./2020-toyota-rav4-xle-awd.json", "./2009-gmc-yukon-xl-denali-awd.json", "./icons/icon-192.png", "./icons/icon-512.png"];
+const CACHE = "garage-pm-v2";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./vehicles-index.json", "./data/tiguan.json", "./data/rav4.json", "./data/yukon.json", "./icons/icon-192.png", "./icons/icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => { if (event.request.method !== "GET") return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { const copy = response.clone(); if (new URL(event.request.url).origin === self.location.origin) caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match("./index.html")))) });
